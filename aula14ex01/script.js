ec21d573a4422b99aacedcdc0cb32f0c91807670
AiltonCh <ailtondark30@gmail.com>
2022-10-26 05:06:59 -0300

    ex016

---
+++ aula14ex01/script.js
@@ -0,0 +1,25 @@
+function contador (){
+    var inicio = document.querySelector('input#ini')
+    var fim = document.getElementById('fim')
+    var salto = document.querySelector('input#pas')
+    var res = document.querySelector('div#res')
+    var incont = Number(inicio.value)
+    var ficont = Number(fim.value)
+    var sacont = Number(salto.value)
+    
+    if (inicio.value.length == 0 || fim.value.length == 0) {
+        window.alert("[ERROR] faltam dados!")
+    } else {
+        res.innerHTML = "Contando:"
+        for (var cont = incont; cont <=ficont; cont += sacont){
+            res.innerHTML += `${cont}🪙 `
+        }
+    }
+}
+
+
+
+
+
+
+
